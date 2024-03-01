b598c391b5c69fbbe486af99e93a5fe85e56acc4
sanjay <sanjay@gmail.com>
2024-03-01 14:48:18 +0530

    Freight master add/edit screen ui design

--- src/app/screens/master/freight/freight.component.ts
+++ src/app/screens/master/freight/freight.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-freight',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './freight.component.scss'
 })
 export class FreightComponent {
+  constructor(private router : Router){}
+
   isFilters : boolean = true;
-}
+
+  onCreateFreight(){
+    this.router.navigate(['master/addEditFreight'])
+  }
+} 
