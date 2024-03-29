5bc4cc501888fd834ccd1f9598ed74556fe89d55
jaiprakash_di <jai.prakash@diverseinfotech.com>
2024-03-29 20:23:12 +0530

    Transaction type UI integration

--- src/app/screens/master/transaction-type/add-edit-transaction-type/add-edit-transaction-type.component.ts
+++ src/app/screens/master/transaction-type/add-edit-transaction-type/add-edit-transaction-type.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TransactionTypesService } from '../../../../core/service/transactionTypes.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-add-edit-transaction-type',
   templateUrl: './add-edit-transaction-type.component.html',
   styleUrl: './add-edit-transaction-type.component.scss'
 })
-export class AddEditTransactionTypeComponent {
-  constructor(private router: Router) {}
+export class AddEditTransactionTypeComponent implements OnInit {
+  constructor(private router: Router,
+    private transactionTypesService : TransactionTypesService,
+    private toastr : ToastrService,
+    private _Activatedroute : ActivatedRoute) {}
+
+  queryData : any = '';
+  transactionData : any;
+  loadSpinner : boolean = true;
+
+  ngOnInit(): void {
+    this.queryData = this._Activatedroute.snapshot.paramMap.get("transactionId");   
+    this.getTransactionData(this.queryData);
+  }
+
+  getTransactionData(transactionId: string) {
+    this.transactionTypesService.getTransactionTypeData(transactionId).subscribe((response: any) => {
+      this.transactionData = response;
+      this.loadSpinner = false;
+    }, error => {
+      this.toastr.error(error.statusText, error.status);
+      this.loadSpinner = false;
+    })
+  }
+
+  onPressSave(){
+    this.loadSpinner = true;
+    let data = {
+      code: this.transactionData.code,
+      name: this.transactionData.name,
+      interfaceTxnTypeId: 0,
+      glSubCategoryId: 0,
+      status: this.transactionData.status,
+      attribute1: "",
+      attribute2: "",
+      attribute3: "",
+      attribute4: "",
+      actionBy: ""
+    }
+    this.transactionTypesService.updateTransaction(this.queryData, data).subscribe((response: any) => {
+      this.transactionData = response;
+      this.loadSpinner = false;
+      this.toastr.success('Transaction Updated Successfully');
+    }, error => {
+      this.toastr.error(error.statusText, error.status);
+      this.loadSpinner = false;
+    })
+  }
 
   onCancelPress(){
     this.router.navigate(['/master/transactionTypes'])
--- src/app/screens/master/transaction-type/components/transaction-type-grid-table/transaction-grid-table.component.ts
+++ src/app/screens/master/transaction-type/components/transaction-type-grid-table/transaction-grid-table.component.ts
@@ -1,15 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, SimpleChanges, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { TransactionTypesService } from '../../../../../core/service/transactionTypes.service';
+import { ToastrService } from 'ngx-toastr';
+import { TransactionTypeListModel } from '../../../../../core/model/transactionType.model';
 
 @Component({
   selector: 'app-transaction-grid-table',
   templateUrl: './transaction-grid-table.component.html',
   styleUrl: './transaction-grid-table.component.scss'
 })
-export class TransactionGridTableComponent {
-  constructor(private router : Router){}
+export class TransactionGridTableComponent implements OnInit{
+  constructor(private router : Router,
+    private transactionTypesService : TransactionTypesService,
+    private toastr : ToastrService
+    ){}
 
-  onGoToEditTransaction(){
-    this.router.navigate(['master/addEditTransactionTypes'])
+    transactionTypesList : TransactionTypeListModel[] = [];
+    transactionTypesListOrg : TransactionTypeListModel[] = [];
+    loadSpinner : boolean = true;
+    @Input() filterKeyword!: string;
+
+  ngOnInit(): void {
+    this.getAllTransactionTypes()
+  }
+
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.transactionTypesListOrg && this.transactionTypesListOrg.length && changes['filterKeyword'].currentValue){
+      this.transactionTypesList = this.transactionTypesListOrg.filter((e:any) =>e.code.toLowerCase().indexOf(changes['filterKeyword'].currentValue.toLowerCase()) !== -1)
+    }
+    else if(this.transactionTypesListOrg && this.transactionTypesListOrg.length && !changes['filterKeyword'].currentValue){
+      this.transactionTypesList = this.transactionTypesListOrg;
+    }
+  }
+
+  getAllTransactionTypes(){
+    let data = {
+      "code" : ''
+    }
+    this.transactionTypesService.getTransactionTypes(data).subscribe((response:any) => {
+      this.transactionTypesList = response.transactionTypes;
+      this.transactionTypesListOrg = response.transactionTypes;
+      this.loadSpinner = false;
+    }, error => {
+      this.toastr.error(error.statusText, error.status);
+      this.loadSpinner = false;
+    })
+  }
+
+  onGoToEditTransaction(transactionData:TransactionTypeListModel){
+    this.router.navigate(['master/addEditTransactionTypes',  transactionData.id]);
   }
 }
