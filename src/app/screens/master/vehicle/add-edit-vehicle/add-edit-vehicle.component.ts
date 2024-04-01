f3bd35685f4ddf72fff8705f33748f69a7083f6b
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-04-01 19:35:28 +0530

    form control added

--- src/app/screens/master/vehicle/add-edit-vehicle/add-edit-vehicle.component.ts
+++ src/app/screens/master/vehicle/add-edit-vehicle/add-edit-vehicle.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { BaseService } from '../../../../core/service/base.service';
+import { VehicleService } from '../../../../core/service/vehicle.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { VehicleDataModel } from '../../../../core/model/vehicle.model';
 
 @Component({
   selector: 'app-add-edit-vehicle',
@@ -7,9 +12,31 @@ import { Router } from '@angular/router';
   styleUrl: './add-edit-vehicle.component.scss'
 })
 export class AddEditVehicleComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+    private _route: ActivatedRoute,
+    private vehicleService: VehicleService,
+    private toastr: ToastrService,
+    private baseService: BaseService) { }
 
-  onCancelPress(){
+  mode: any = 'create';
+  vehicleId: any;
+  vehicleData!: VehicleDataModel
+  vehiclesList: any = [];
+  lookupId: number = 4;
+  locationId: number = 0;
+  loadSpinner: boolean = true;
+  vehicleForm = new FormGroup({
+    vehicleNumber: new FormControl('', [Validators.required]),
+    tarnsporterName: new FormControl('', [Validators.required]),
+    vehicleSize: new FormControl('', [Validators.required]),
+    vehicleCondition: new FormControl('', [Validators.required]),
+    remarks: new FormControl('', [Validators.required]),
+    vehicleStatus: new FormControl('', [Validators.required]),
+    createdBy: new FormControl('', [Validators.required]),
+  })
+
+
+  onCancelPress() {
     this.router.navigate(['master/vehicle']);
   }
 }
