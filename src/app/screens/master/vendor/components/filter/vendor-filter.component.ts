ad3288df8a62282d176eaef406f576d8035866fe
sanjay <sanjay@gmail.com>
2024-04-04 18:23:08 +0530

    Custom filter search dropdown

--- src/app/screens/master/vendor/components/filter/vendor-filter.component.ts
+++ src/app/screens/master/vendor/components/filter/vendor-filter.component.ts
@@ -1,16 +1,16 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, OnInit, Output, ViewChild } from '@angular/core';
 import { VendorService } from '../../../../../core/service/vendor.service';
 import { ToastrService } from 'ngx-toastr';
-
 
 @Component({
   selector: 'app-vendor-filter',
   templateUrl: './vendor-filter.component.html',
-  styleUrl: './vendor-filter.component.scss'
+  styleUrls: ['./vendor-filter.component.scss']
 })
 export class VendorFilterComponent implements OnInit {
   constructor(
     private vendorService: VendorService,
+    private elementRef: ElementRef,
     private toastr: ToastrService
   ) { }
 
@@ -18,7 +18,9 @@ export class VendorFilterComponent implements OnInit {
   vendorCod!: string | undefined;
   vendorsList: any;
   vendorNam!: string;
-
+  allVendorNames: string[] = [];
+  filteredVendors: string[] = [];
+  showSuggestions: boolean = false;
 
   ngOnInit(): void {
     this.getAllVendorsListInit();
@@ -31,6 +33,7 @@ export class VendorFilterComponent implements OnInit {
     }
     this.vendorService.getVendors(data).subscribe((response: any) => {
       this.vendorsList = response.vendors;
+      this.allVendorNames = response.vendors.map((vendor: any) => vendor.vendorName);
     }, error => {
       this.toastr.error(error.statusText, error.status);
     });
@@ -42,15 +45,36 @@ export class VendorFilterComponent implements OnInit {
       "vendorName": this.vendorNam || ""
     }
     this.vendorFilterData.emit(obj)
-
   }
-  onClearFilter(){
+
+  onClearFilter() {
     this.vendorCod = undefined;
     this.vendorNam = '';
     let obj = {
-      vendorCod : '',
-      vendorNam : ''
+      vendorCod: '',
+      vendorNam: ''
     }
     this.vendorFilterData.emit(obj)
+  }
+
+  // Filters vendor names based on user input and shows/hides suggestions accordingly.
+  onVendorNameInput(inputText: string) {
+    this.filteredVendors = this.allVendorNames.filter(name => name.toLowerCase().includes(inputText.toLowerCase()));
+    this.filteredVendors.length ? this.showSuggestions = true : this.showSuggestions = false;
+  }
+ 
+  // Sets the selected vendor name, clears the filtered vendors list, and hides the suggestion dropdown.
+  selectSuggestion(vendor: string) {
+    this.vendorNam = vendor;
+    this.filteredVendors = [];
+    this.showSuggestions = false;
+  }
+
+  // Click occurred outside the search dropdown, close it
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: any) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.showSuggestions = false;
+    }
   }
 }
