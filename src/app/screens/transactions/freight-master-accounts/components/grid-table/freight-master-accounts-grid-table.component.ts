be8854e28fb8b6a3b5dd2ca91cd505041d7d9a53
sanjay <sanjay@gmail.com>
2024-05-04 22:51:52 +0530

    Freight Accont Approval API Integration

--- src/app/screens/transactions/freight-master-accounts/components/grid-table/freight-master-accounts-grid-table.component.ts
+++ src/app/screens/transactions/freight-master-accounts/components/grid-table/freight-master-accounts-grid-table.component.ts
@@ -1,14 +1,103 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Component, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgbPopover } from "@ng-bootstrap/ng-bootstrap";
+import { FreightService } from '../../../../../core/service/freight.service';
+import { ToastrService } from 'ngx-toastr';
+import { CommonTransactionService } from '../../../../../core/service/commonTransaction.service';
+import { FreightDataModel } from '../../../../../core/model/masterModels.model';
 
 @Component({
   selector: 'app-freight-master-accounts-grid-table',
   templateUrl: './freight-master-accounts-grid-table.component.html',
   styleUrl: './freight-master-accounts-grid-table.component.scss'
 })
-export class FreightMasterAccountsGridTableComponent {
-  constructor(private router: Router,
-    private modalService: NgbModal) {}
+export class FreightMasterAccountsGridTableComponent implements OnInit {
+  constructor(private freightService: FreightService, private toastr: ToastrService, private commonTransactionService: CommonTransactionService, private _Activatedroute: ActivatedRoute) { }
+  @ViewChild('approvePopover') approvePopover!: NgbPopover;
+  @ViewChild('rejectPopover') rejectPopover!: NgbPopover;
+  @Input()
+  searchedFreight!: any;
+  freightList: any;
+  loadSpinner: boolean = true;
+  freightData!: FreightDataModel;
+  selectedFreightId: number = 0;
+
+  ngOnInit(): void {
+    this.getAllFreightListInit();
+  }
+
+  //SORTING DATA FROM FILTER CHANGES
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['searchedFreight'].currentValue) {
+      this.getAllFreightListInit();
+    } else if (changes['searchedFreight'].firstChange === false && changes['searchedFreight'].currentValue === undefined) {
+      this.getAllFreightListInit();
+    }
+  }
+
+  //GETTINGS FREIGHTS LISTING ON PAGE LOAD
+  getAllFreightListInit() {
+    let data = {
+      "screenCode": 103, //Freight Account Screen Code
+      "freightCode": '',
+      "sourceId": 0,
+      "vehicleSizeId": 0
+    }
+    this.freightService.getFreightsList(data).subscribe((response: any) => {
+      this.freightList = response.freights;
+      console.log(this.freightList, "this.freightList");
+      this.selectFreight(this.selectedFreightId);
+
+      this.loadSpinner = false;
+    }, error => {
+      this.toastr.error(error.statusText, error.status);
+      this.loadSpinner = false;
+    })
+  }
+
+  selectFreight(freightId: number) {
+    this.selectedFreightId = freightId;
+  }
+
+  openPopover(popover: NgbPopover) {
+    popover.open();
+  }
+
+  closePopover(popover: NgbPopover) {
+    popover.close();
+  }
+
+  approveFreight(remarks: string, approvePopover: NgbPopover) {
+    const payload = this.approvalPayload('Approved', remarks);
+    this.updateStatus(payload, approvePopover);
+  }
+
+  rejectFreight(remarks: string, rejectPopover: NgbPopover) {
+    const payload = this.approvalPayload('Rejected', remarks);
+    this.updateStatus(payload, rejectPopover);
+  }
+
+  approvalPayload(status: string, remarks: string): any {
+    return {
+      "approvalLevel": "Accounts",
+      "status": status,
+      "remarks": remarks,
+      "actionBy": 1,
+      "transactionCode": 201 // for Account freight
+    };
+  }
+
+  updateStatus(payload: any, popover: NgbPopover) {
+    this.commonTransactionService.updateStatus(this.selectedFreightId, payload).subscribe((response: any) => {
+      this.freightData = response;
+      this.loadSpinner = false;
+      this.toastr.success('Freight Approval Updated Successfully');
+      popover.close();
+      this.getAllFreightListInit();
+    }, error => {
+      this.toastr.error(error.statusText, error.status);
+      this.loadSpinner = false;
+    });
+  }
 
 }
