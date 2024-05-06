06883bb37873b5b018ace418bb41d1e464466999
sanjay <sanjay@gmail.com>
2024-05-06 16:38:19 +0530

    Integration Of Point Master Account API

--- src/app/screens/transactions/point-master-accounts/components/grid-table/point-master-accounts-grid-table.component.ts
+++ src/app/screens/transactions/point-master-accounts/components/grid-table/point-master-accounts-grid-table.component.ts
@@ -1,14 +1,98 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Component, Input, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NgbPopover } from "@ng-bootstrap/ng-bootstrap";
+import { ToastrService } from 'ngx-toastr';
+import { CommonTransactionService } from '../../../../../core/service/commonTransaction.service';
+import { PointChargeDataModel } from '../../../../../core/model/masterModels.model';
+import { PointChargeService } from '../../../../../core/service/point-charge.service';
 
 @Component({
   selector: 'app-point-master-accounts-grid-table',
   templateUrl: './point-master-accounts-grid-table.component.html',
   styleUrl: './point-master-accounts-grid-table.component.scss'
 })
-export class PointMasterAccountsGridTableComponent {
-  constructor(private router: Router,
-    private modalService: NgbModal) {}
+export class PointMasterAccountsGridTableComponent implements OnInit{
+  constructor(private pointChargeService: PointChargeService, private toastr: ToastrService, private commonTransactionService: CommonTransactionService, private _Activatedroute: ActivatedRoute) { }
+  @Input()
+  searchedPoint!: any;
+  pointChargesList: any;
+  pointChargeListOrg: any;
+  loadSpinner: boolean = true;
+  pointData!: PointChargeDataModel;
+  selectedPointId: number = 0;
+
+  ngOnInit(): void {
+    this.getAllPointChargesList();
+  }
+
+    //SORTING DATA FROM FILTER CHANGES
+    ngOnChanges(changes: SimpleChanges): void {
+      if(changes['searchedPoint'].currentValue){
+        this.getAllPointChargesList();
+      } else if(changes['searchedPoint'].firstChange === false && changes['searchedPoint'].currentValue === undefined){
+        this.getAllPointChargesList();
+      }
+    }
+
+    // GET ALL POINT CHARGE
+    getAllPointChargesList() {
+      let data = {
+        "screenCode": 103,
+        "pointName": ''
+      }
+      this.pointChargeService.getPointCharges(data).subscribe((response: any) => {
+        this.pointChargesList = response.pointCharges;
+        this.pointChargeListOrg = response.pointCharges;
+        this.loadSpinner = false;
+      }, error => {
+        this.toastr.error(error.statusText, error.status);
+        this.loadSpinner = false;
+      })
+    }
+
+  selectPoint(pointId: number) {
+    this.selectedPointId = pointId;
+  }
+
+  openPopover(popover: NgbPopover) {
+    popover.open();
+  }
+
+  closePopover(popover: NgbPopover) {
+    popover.close();
+  }
+
+  approvePoint(remarks: string, approvePopover: NgbPopover) {
+    const payload = this.approvalPayload('Approved', remarks);
+    this.updateStatus(payload, approvePopover);
+  }
+
+  rejectPoint(remarks: string, rejectPopover: NgbPopover) {
+    const payload = this.approvalPayload('Rejected', remarks);
+    this.updateStatus(payload, rejectPopover);
+  }
+
+  approvalPayload(status: string, remarks: string): any {
+    return {
+      "approvalLevel": "Account",
+      "status": status,
+      "remarks": remarks,
+      "actionBy": 1,
+      "transactionCode": 202 // for Account Point Charge
+    };
+  }
+
+  updateStatus(payload: any, popover: NgbPopover) {
+    this.commonTransactionService.updateStatus(this.selectedPointId, payload).subscribe((response: any) => {
+      this.pointData = response;
+      this.loadSpinner = false;
+      this.toastr.success('Point Approval Updated Successfully');
+      popover.close();
+      this.getAllPointChargesList();
+    }, error => {
+      this.toastr.error(error.statusText, error.status);
+      this.loadSpinner = false;
+    });
+  }
 
 }
