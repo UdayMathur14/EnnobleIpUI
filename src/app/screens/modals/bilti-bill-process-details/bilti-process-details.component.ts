a17bd2fcac8ba453894ebdbc0adf8892f7217ca8
sanjay <sanjay@gmail.com>
2024-05-16 12:02:53 +0530

    Calculation for Charges by Vendor

--- src/app/screens/modals/bilti-bill-process-details/bilti-process-details.component.ts
+++ src/app/screens/modals/bilti-bill-process-details/bilti-process-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { BiltiBillProcessService } from '../../../core/service/biltiBillProcess.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, FormControl, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-bilti-process-details-modal',
@@ -12,6 +12,16 @@ export class BiltiProcessDetailsModalComponent implements OnInit {
   @Input() biltiProcess: any;
   biltiBillProcess!: FormGroup;
   biltiBillProcessData: any;
+
+  // Totals
+  totalFreightCharge: number = 0;
+  totalPointCharge: number = 0;
+  totalDetentionCharge: number = 0;
+  totalOverloadCharge: number = 0;
+  totalTollTax: number = 0;
+  totalUnloadingCharge: number = 0;
+  totalOtherCharges: number = 0;
+
   constructor(
     public activeModal: NgbActiveModal,
     private biltiBillService: BiltiBillProcessService,
@@ -19,15 +29,11 @@ export class BiltiProcessDetailsModalComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    // logs -->
-    // console.log(this.biltiProcess, "modal data");
-    // console.log(this.biltiProcess.id, "bilti Id");
-    // console.log(this.biltiBillProcessData.biltiCreationLineItemDetails?.supplierDetail?.vendorName,"vendor Name");
-    //<----
-    this.getBiltiBillProcessbyId();
+
     this.initForm();
+    this.getBiltiBillProcessbyId();
   }
-  
+
   initForm(): void {
     this.biltiBillProcess = this.formBuilder.group({
       biltiNumber: [''],
@@ -39,7 +45,27 @@ export class BiltiProcessDetailsModalComponent implements OnInit {
       penaltyAmount: [''],
       penaltyReason: [''],
       excessAmount: [''],
-      excessReason: ['']
+      excessReason: [''],
+      biltiCreationLineItemDetails: this.formBuilder.array([])
+    });
+  }
+
+  get biltiCreationLineItemDetails(): FormArray {
+    return this.biltiBillProcess.get('biltiCreationLineItemDetails') as FormArray;
+  }
+
+  createLineItem(item: any): FormGroup {
+    return this.formBuilder.group({
+      documentNumber: [item.fRMTransactionDetails?.documentNumber || ''],
+      vendorName: [item.supplierDetail?.vendorName || ''],
+      freightCharge: [item.freightCharge || 0],
+      pointCharge: [item.pointCharge || 0],
+      detentionCharge: [item.detentionCharge || 0],
+      overloadCharge: [item.overloadCharge || 0],
+      tollTax: [item.tollTax || 0],
+      unloadingCharge: [item.unloadingCharge || 0],
+      otherCharges: [item.otherCharges || 0],
+      remarks: [item.remarks || '']
     });
   }
 
@@ -47,23 +73,20 @@ export class BiltiProcessDetailsModalComponent implements OnInit {
     this.biltiBillService.getBiltiBillProcessbyId(this.biltiProcess.id).subscribe((response => {
       console.log(response, "response");
       this.biltiBillProcessData = response;
-      console.log(this.biltiBillProcessData.biltiNumber,"this.biltiBillProcessData");
-     
-    console.log(this.biltiBillProcessData.biltiCreationLineItemDetails?.supplierDetail?.vendorName,"vendor Name");
-      
-      this.populateForm();
+      console.log(this.biltiBillProcessData.biltiNumber, "this.biltiBillProcessData");
 
+      console.log(this.biltiBillProcessData.biltiCreationLineItemDetails?.supplierDetail?.vendorName, "vendor Name");
+
+      this.populateForm();
     }))
   }
 
   populateForm(): void {
-    // const vendorNames = this.biltiBillProcessData.biltiCreationLineItemDetails
-    //   .map((item: any) => item.supplierDetail.vendorName)
-    //   ;
+
     this.biltiBillProcess.patchValue({
       biltiNumber: this.biltiBillProcessData?.biltiNumber,
       creationDate: this.biltiBillProcessData?.creationDate,
-      adviceType: this.biltiBillProcessData.transactionTypeDetails?.name, 
+      adviceType: this.biltiBillProcessData.transactionTypeDetails?.name,
       // vendorName: vendorNames,
       freightAmount: this.biltiBillProcessData.freightDetails?.freightAmount,
       penaltyAmount: this.biltiBillProcessData.biltiBillProcessModel?.penaltyAmount,
@@ -71,6 +94,52 @@ export class BiltiProcessDetailsModalComponent implements OnInit {
       excessAmount: this.biltiBillProcessData.biltiBillProcessModel?.excessAmount,
       excessReason: this.biltiBillProcessData.biltiBillProcessModel?.excessReason
     });
+    const biltiCreationLineItemDetailsData = this.biltiBillProcessData?.biltiCreationLineItemDetails;
+
+    if (biltiCreationLineItemDetailsData) {
+      const formArray = this.biltiBillProcess.get('biltiCreationLineItemDetails') as FormArray;
+
+      // Clear existing form array controls
+      formArray.clear();
+
+      // Populate form array with new form groups
+      biltiCreationLineItemDetailsData.forEach((item: any) => {
+        formArray.push(this.createLineItemFormGroup(item));
+      });
+    }
+  }
+
+  createLineItemFormGroup(item: any): FormGroup {
+    return this.formBuilder.group({
+      // Define your form controls here and set their initial values
+      documentNumber: [item.fRMTransactionDetails?.documentNumber || ''],
+      vendorName: [item.supplierDetail?.vendorName || ''],
+      freightCharge: [item.freightCharge || ''],
+      pointCharge: [item.pointCharge || ''],
+      detentionCharge: [item.detentionCharge || ''],
+      overloadCharge: [item.overloadCharge || ''],
+      tollTax: [item.tollTax || ''],
+      unloadingCharge: [item.unloadingCharge || ''],
+      otherCharges: [item.otherCharges || ''],
+      remarks: [item.remarks || '']
+    });
+  }
+
+  calculateTotals(): void {
+    this.totalFreightCharge = this.sumColumn('freightCharge');
+    this.totalPointCharge = this.sumColumn('pointCharge');
+    this.totalDetentionCharge = this.sumColumn('detentionCharge');
+    this.totalOverloadCharge = this.sumColumn('overloadCharge');
+    this.totalTollTax = this.sumColumn('tollTax');
+    this.totalUnloadingCharge = this.sumColumn('unloadingCharge');
+    this.totalOtherCharges = this.sumColumn('otherCharges');
+  }
+
+  sumColumn(column: string): number {
+    return this.biltiCreationLineItemDetails.controls.reduce((total: number, control: AbstractControl) => {
+      const value = parseFloat((control as FormGroup).get(column)?.value) || 0;
+      return total + value;
+    }, 0);
   }
 
 }
