145b2221d0cb9f52c4b7d78541929c3173752241
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-05-18 22:33:40 +0530

    filter in approval by accounts

--- src/app/screens/transactions/approval-accounts/components/grid-table/approval-accounts-grid-table.component.ts
+++ src/app/screens/transactions/approval-accounts/components/grid-table/approval-accounts-grid-table.component.ts
@@ -1,9 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AccountMaterialBiltiProcessDetailsModalComponent } from '../../../../modals/accMat-bilti-bill-process-details/accMat-bilti-process-details.component';
-import { BiltiBillProcessService } from '../../../../../core/service/biltiBillProcess.service';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-approval-accounts-grid-table',
@@ -11,17 +9,15 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './approval-accounts-grid-table.component.scss',
 })
 export class ApprovalAccountsGridTableComponent implements OnInit {
-  biltiBillProcessList: any = [];
-  loadSpinner: boolean = true;
+
+  @Input() biltiBillProcess: any = [];
   constructor(
     private router: Router,
     private modalService: NgbModal,
-    private biltiBillService: BiltiBillProcessService,
-    private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
-    this.getAllBiltiProcessList();
+
   }
 
   onPreviewBiltiDetails() {
@@ -40,29 +36,6 @@ export class ApprovalAccountsGridTableComponent implements OnInit {
         }
       },
       (reason) => {}
-    );
-  }
-
-  getAllBiltiProcessList() {
-    this.loadSpinner = true;
-    const data = {
-      screenCode: 305,
-      fromDate: '2024-04-18T12:19:20.868Z',
-      toDate: '2024-05-18T12:19:20.868Z',
-      batchNumber: '',
-      adviceType: '',
-      batchName: '',
-      biltiNumber: '',
-    };
-    this.biltiBillService.getBiltiBillProcess(data).subscribe(
-      (response: any) => {
-        this.biltiBillProcessList = response.biltiBillProcess;
-        this.loadSpinner = false;
-      },
-      (error) => {
-        this.toastr.error(error.statusText, error.status);
-        this.loadSpinner = false;
-      }
     );
   }
 }
