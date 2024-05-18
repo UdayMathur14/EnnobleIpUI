3b71a9180936b38da207bd48b6a63273049f51cd
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-05-18 19:01:47 +0530

    accounts approval listing api integration

--- src/app/screens/transactions/approval-accounts/components/grid-table/approval-accounts-grid-table.component.ts
+++ src/app/screens/transactions/approval-accounts/components/grid-table/approval-accounts-grid-table.component.ts
@@ -1,33 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AccountMaterialBiltiProcessDetailsModalComponent } from '../../../../modals/accMat-bilti-bill-process-details/accMat-bilti-process-details.component';
+import { BiltiBillProcessService } from '../../../../../core/service/biltiBillProcess.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-approval-accounts-grid-table',
   templateUrl: './approval-accounts-grid-table.component.html',
-  styleUrl: './approval-accounts-grid-table.component.scss'
+  styleUrl: './approval-accounts-grid-table.component.scss',
 })
-export class ApprovalAccountsGridTableComponent {
-  constructor(private router: Router,
-    private modalService: NgbModal) {}
+export class ApprovalAccountsGridTableComponent implements OnInit {
+  biltiBillProcessList: any = [];
+  loadSpinner: boolean = true;
+  constructor(
+    private router: Router,
+    private modalService: NgbModal,
+    private biltiBillService: BiltiBillProcessService,
+    private toastr: ToastrService
+  ) {}
 
-    onPreviewBiltiDetails(){
-      let deliveryNoteModal = this.modalService.open(AccountMaterialBiltiProcessDetailsModalComponent, {
-        size: "xl",
-        backdrop: "static",
-      });
-      // deliveryNoteModal.componentInstance.data = data;
-  
-      deliveryNoteModal.result.then(
-        (result) => {
-          if (result) {
-  
-          }
-        },
-        (reason) => {
+  ngOnInit(): void {
+    this.getAllBiltiProcessList();
+  }
+
+  onPreviewBiltiDetails() {
+    let deliveryNoteModal = this.modalService.open(
+      AccountMaterialBiltiProcessDetailsModalComponent,
+      {
+        size: 'xl',
+        backdrop: 'static',
+      }
+    );
+    // deliveryNoteModal.componentInstance.data = data;
+
+    deliveryNoteModal.result.then(
+      (result) => {
+        if (result) {
         }
-      );
-    }
+      },
+      (reason) => {}
+    );
+  }
 
+  getAllBiltiProcessList() {
+    this.loadSpinner = true;
+    const data = {
+      screenCode: 305,
+      fromDate: '2024-04-18T12:19:20.868Z',
+      toDate: '2024-05-18T12:19:20.868Z',
+      batchNumber: '',
+      adviceType: '',
+      batchName: '',
+      biltiNumber: '',
+    };
+    this.biltiBillService.getBiltiBillProcess(data).subscribe(
+      (response: any) => {
+        this.biltiBillProcessList = response.biltiBillProcess;
+        this.loadSpinner = false;
+      },
+      (error) => {
+        this.toastr.error(error.statusText, error.status);
+        this.loadSpinner = false;
+      }
+    );
+  }
 }
