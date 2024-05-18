580ec243d50983c5b7187bb05bcc8b47eee45ef5
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-05-18 19:04:07 +0530

    approval and checked by material grid table api integration

--- src/app/screens/transactions/approval-material-head/components/grid-table/approval-material-head-grid-table.component.ts
+++ src/app/screens/transactions/approval-material-head/components/grid-table/approval-material-head-grid-table.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ApprovalMaterialHeadModalComponent } from '../../../../modals/approval-material-head/approval-material-head.component';
+import { ToastrService } from 'ngx-toastr';
+import { BiltiBillProcessService } from '../../../../../core/service/biltiBillProcess.service';
 
 @Component({
   selector: 'app-approval-material-head-grid-table',
   templateUrl: './approval-material-head-grid-table.component.html',
   styleUrl: './approval-material-head-grid-table.component.scss'
 })
-export class ApprovalMaterialHeadGridTableComponent {
+export class ApprovalMaterialHeadGridTableComponent implements OnInit {
+
+  biltiBillProcessList: any = [];
+  loadSpinner: boolean = true;
+
   constructor(private router: Router,
-    private modalService: NgbModal) {}
+    private modalService: NgbModal,
+    private biltiBillService: BiltiBillProcessService,
+    private toastr: ToastrService) {}
+
+    ngOnInit(): void {
+      this.getAllBiltiProcessList();
+    }
 
     onPreviewApprovalMatHead(){
       let deliveryNoteModal = this.modalService.open(ApprovalMaterialHeadModalComponent, {
@@ -26,6 +38,29 @@ export class ApprovalMaterialHeadGridTableComponent {
           }
         },
         (reason) => {
+        }
+      );
+    }
+
+    getAllBiltiProcessList() {
+      this.loadSpinner = true;
+      const data = {
+        screenCode: 304,
+        fromDate: '2024-04-18T12:19:20.868Z',
+        toDate: '2024-05-18T12:19:20.868Z',
+        batchNumber: '',
+        adviceType: '',
+        batchName: '',
+        biltiNumber: '',
+      };
+      this.biltiBillService.getBiltiBillProcess(data).subscribe(
+        (response: any) => {
+          this.biltiBillProcessList = response.biltiBillProcess;
+          this.loadSpinner = false;
+        },
+        (error) => {
+          this.toastr.error(error.statusText, error.status);
+          this.loadSpinner = false;
         }
       );
     }
