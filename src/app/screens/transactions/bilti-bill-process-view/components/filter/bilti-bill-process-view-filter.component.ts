de17a2b62e41904f3d0d99befbdb5417760e5fa5
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-05-20 00:58:34 +0530

    bilti bill process view

--- src/app/screens/transactions/bilti-bill-process-view/components/filter/bilti-bill-process-view-filter.component.ts
+++ src/app/screens/transactions/bilti-bill-process-view/components/filter/bilti-bill-process-view-filter.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild, inject } from '@angular/core';
+import { NgbDateStruct, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-bilti-bill-process-view-filter',
@@ -6,5 +8,46 @@ import { Component } from '@angular/core';
   styleUrl: './bilti-bill-process-view-filter.component.scss'
 })
 export class BiltiBillProcessViewFilterComponent {
+  fromDate!: NgbDateStruct;
+  toDate!: NgbDateStruct;
+  model!: NgbDateStruct;
+  selectedFromDate: string = '';
+  selectedToDate: string = '';
+  batchName: any = undefined;
+  biltiNumber: any = undefined;
+  batchNames: string[] = [];
+  today = inject(NgbCalendar).getToday();
+  loadSpinner: boolean = true;
+  @ViewChild('batchNameInput') batchNameInput!: ElementRef<HTMLInputElement>;
 
+  @Output() filterSearchObj: EventEmitter<any> = new EventEmitter();
+  constructor(
+    private toastr: ToastrService,
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  onDateSelect(type: string, e: any) {
+    const month = Number(e.month) < 10 ? '0' + e.month : e.month;
+    const day = Number(e.day) < 10 ? '0' + e.day : e.day;
+    if (type === 'fromDate') {
+      this.selectedFromDate = e.year + '-' + month.toString() + '-' + day.toString();
+    } else {
+      this.selectedToDate = e.year + '-' + month.toString() + '-' + day.toString();
+    }
+  }
+
+
+  handleSearch() {
+    const batchNameValue = this.batchNameInput ? this.batchNameInput.nativeElement.value : '';
+    const filterObj = {
+      "biltiNumber": this.biltiNumber || "",
+      "batchNumber": batchNameValue || "",
+      "fromDate": this.selectedFromDate || "2000-01-01",
+      "toDate": this.selectedToDate || new Date().getFullYear() + '-' + ("0" + (new Date().getMonth() + 1)).slice(-2) + '-' + ("0" + (new Date().getDate())).slice(-2)
+    }
+    this.filterSearchObj.emit(filterObj)
+  }
 }
