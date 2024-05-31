a0272d25aadecd1ae2ebb33f30a11f5ef194d9d4
sanjay <sanjay@gmail.com>
2024-05-31 18:02:39 +0530

    xlsx implementation in Plant Master

--- src/app/core/utilities/common.ts
+++ src/app/core/utilities/common.ts
@@ -132,4 +132,10 @@ export class CommonUtility {
           return sortDirection === 'asc' ? comparison : -comparison;
         });
       }
+      static camelize(str:string) {
+        if(!str){return "";}
+        return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+            return index === 0 ? word.toLowerCase() : word.toUpperCase();
+        }).replace(/\s+/g, '');
+    }
 }
