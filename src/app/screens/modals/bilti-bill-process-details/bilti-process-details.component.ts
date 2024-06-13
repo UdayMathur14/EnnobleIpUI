8f81d3f714a6ed5abcdb95e19f7c7cab86480779
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-06-14 02:48:49 +0530

    amount filed should only accept nos

--- src/app/screens/modals/bilti-bill-process-details/bilti-process-details.component.ts
+++ src/app/screens/modals/bilti-bill-process-details/bilti-process-details.component.ts
@@ -576,4 +576,12 @@ export class BiltiProcessDetailsModalComponent implements OnInit {
   });
 }
 }
+
+validateNo(e: any) {
+  const charCode = e.which ? e.which : e.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+    return false
+  }
+  return true;
+}
 }
