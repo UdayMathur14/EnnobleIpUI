0e5b7abb47f6b674a092ac9ffba672456e40430e
jaiprakash_di <jai.prakash@diverseinfotech.com>
2024-06-15 11:26:39 +0530

    Welcome screen UI design

--- src/app/screens/master/master.component.ts
+++ src/app/screens/master/master.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-master',
   templateUrl: './master.component.html',
   styleUrl: './master.component.scss'
 })
-export class MasterComponent {
+export class MasterComponent implements OnInit{
+  userData : any = localStorage.getItem('logindata');
+  userName : string = "";
+  isFilters : boolean = false;
+  
 
-  constructor() { }
+  constructor(){}
 
+  ngOnInit(): void {
+    debugger
+    this.userName = JSON.parse(this.userData).username;
+  }
 }
