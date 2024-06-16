8750720ff043f88f45a6a6e1eeb64c2065c27cf8
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-06-16 16:26:45 +0530

    pdf download of bilti and dispatch note

--- src/app/screens/modals/bilti-pdf/bilti-pdf.component.ts
+++ src/app/screens/modals/bilti-pdf/bilti-pdf.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-bilti-pdf',
@@ -15,6 +17,48 @@ export class BiltiPdfModalComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  downloadPDF() {
+    const downloadButton = document.getElementById('download-button');
+    const closeButton = document.getElementById('icon-button');
+    const data = document.getElementById('bilti-content') as HTMLElement;
+  
+    if (downloadButton) {
+      downloadButton.style.display = 'none';
+    }
+    if (closeButton) {
+      closeButton.style.display = 'none';
+    }
+  
+    const scale = 3;
+    html2canvas(data, { scale: scale }).then(canvas => {
+      const imgWidth = 208;
+      const pageHeight = 295;
+  
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+  
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const position = 0;
+  
+      if (imgHeight > pageHeight) {
+        const scale = pageHeight / imgHeight;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight * scale);
+      } else {
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      }
+  
+      pdf.save('bilti.pdf');
+  
+      if (downloadButton) {
+        downloadButton.style.display = 'block';
+      }
+      if (closeButton) {
+        closeButton.style.display = 'block';
+      }
+      this.onClose();
+    });
   }
 
   onClose(){
--- src/app/screens/modals/dispatch-note/dispatch-note.component.ts
+++ src/app/screens/modals/dispatch-note/dispatch-note.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-dispatch-note',
@@ -14,5 +16,45 @@ export class DispatchNoteModelComponent {
 
   ngOnInit(){
 
+  }
+
+  downloadPDF() {
+    const downloadButton = document.getElementById('download-button');
+    const closeButton = document.getElementById('icon-button');
+    const data = document.getElementById('dispatch-note-content') as HTMLElement;
+  
+    if (downloadButton) {
+      downloadButton.style.display = 'none';
+    }
+    if (closeButton) {
+      closeButton.style.display = 'none';
+    }
+  
+    const scale = 3;
+    html2canvas(data, { scale: scale }).then(canvas => {
+      const imgWidth = 208;
+      const pageHeight = 295;
+  
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+  
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const position = 0;
+  
+      if (imgHeight > pageHeight) {
+        const scale = pageHeight / imgHeight;
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight * scale);
+      } else {
+        pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      }
+  
+      pdf.save('dispatch-note.pdf');
+
+      this.onClose();
+    });
+  }
+
+  onClose(){
+    this.activeModal.close();
   }
 }
