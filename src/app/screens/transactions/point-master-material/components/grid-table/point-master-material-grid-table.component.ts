35ba55aa7532b0acefd6ed4dcbf084594acb83b7
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-09-02 01:15:46 +0530

    timestamp attachment

--- src/app/screens/transactions/point-master-material/components/grid-table/point-master-material-grid-table.component.ts
+++ src/app/screens/transactions/point-master-material/components/grid-table/point-master-material-grid-table.component.ts
@@ -114,4 +114,36 @@ export class PointMasterMaterialGridTableComponent {
     });
   }
 
+  openPDF(data: any) {
+    this.pointChargeService.getPointChargeData(data?.locationId, data?.id).subscribe(
+        (response: any) => {
+            if (!response.fileData) {
+                this.toastr.error('No PDF is available to download', 'Error');
+                return;
+            }
+
+            const base64Prefix = 'data:application/pdf;base64,';
+            const base64Data = response.fileData.startsWith(base64Prefix) 
+                ? response.fileData.substring(base64Prefix.length) 
+                : response.fileData;
+            const byteCharacters = atob(base64Data);
+            const byteNumbers = new Array(byteCharacters.length);
+            for (let i = 0; i < byteCharacters.length; i++) {
+                byteNumbers[i] = byteCharacters.charCodeAt(i);
+            }
+            const byteArray = new Uint8Array(byteNumbers);
+            const blob = new Blob([byteArray], { type: 'application/pdf' });
+            const url = window.URL.createObjectURL(blob);
+            
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = response.fileName;
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        },
+    );
+}
+
 }
