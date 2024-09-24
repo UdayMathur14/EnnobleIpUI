66a58aa5969fc8c4b9300eaaba7781e72880d5c0
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-09-24 23:24:22 +0530

    validator changes

--- src/app/core/shared/inputDecimalValidator.directive.ts
+++ src/app/core/shared/inputDecimalValidator.directive.ts
@@ -5,22 +5,27 @@ import { Directive, HostListener, ElementRef } from '@angular/core';
 })
 export class InputDecimalValidator {
   private regex: RegExp = new RegExp(/^\d+(\.\d{0,2})?$/);
-  private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Delete'];
+  private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight', 'Delete', '.'];
 
   constructor(private el: ElementRef) { }
 
   @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
+    const current: string = this.el.nativeElement.value;
+
     if (this.specialKeys.indexOf(event.key) !== -1) {
-      return; 
+      if (event.key === '.' && current.includes('.')) {
+        event.preventDefault();
+      }
+      return;
     }
 
     if ((event.ctrlKey || event.metaKey) && (event.key === 'a' || event.key === 'c' || event.key === 'v' || event.key === 'x')) {
       return;
     }
 
-    const current: string = this.el.nativeElement.value;
     const next: string = current.concat(event.key);
+
     if (next && !String(next).match(this.regex)) {
       event.preventDefault();
     }
