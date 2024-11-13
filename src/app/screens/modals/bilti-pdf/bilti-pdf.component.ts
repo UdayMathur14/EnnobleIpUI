4d1e44cc1f72c55d0246d9b42dfa139a7d1a772e
kapildiverseinfotech <161805899+kapildiverseinfotech@users.noreply.github.com>
2024-11-14 00:51:23 +0530

    bilti bill process changes done

--- src/app/screens/modals/bilti-pdf/bilti-pdf.component.ts
+++ src/app/screens/modals/bilti-pdf/bilti-pdf.component.ts
@@ -14,6 +14,7 @@ export class BiltiPdfModalComponent implements OnInit {
   modalRef!: NgbModalRef;
   @Input() biltiData: any;
   loadSpinner: boolean = false;
+  printingDate: string | null = null;
 
   constructor(private activeModal: NgbActiveModal, private pdfService: PdfService){}
 
@@ -23,22 +24,18 @@ console.log(this.biltiData);
   }
 
   downloadPDF() {
-    this.loadSpinner =true;
+    this.loadSpinner = true;
+    this.printingDate = this.formatCurrentDate();
+  
     const downloadButton = document.getElementById('download-button');
     const closeButton = document.getElementById('icon-button');
   
-    if (downloadButton) {
-      downloadButton.style.display = 'none';
-    }
-    if (closeButton) {
-      closeButton.style.display = 'none';
-    }
+    if (downloadButton) downloadButton.style.display = 'none';
+    if (closeButton) closeButton.style.display = 'none';
+  
     const data = document.getElementById('bilti-content');
-
-    if (!data) {
-      return;
-    }
-
+    if (!data) return;
+  
     const options = {
       scale: 3,
       useCORS: true,
@@ -47,12 +44,11 @@ console.log(this.biltiData);
     html2canvas(data, options).then((canvas: HTMLCanvasElement) => {
       const imgData = canvas.toDataURL('image/png', 1.0);
       const pdf = new jsPDF('p', 'mm', 'a4');
-
+  
       const imgWidth = 210;
       const pageHeight = 295;
       const imgHeight = (canvas.height * imgWidth) / canvas.width;
       let heightLeft = imgHeight;
-  
       let position = 0;
   
       while (heightLeft >= 0) {
@@ -63,10 +59,27 @@ console.log(this.biltiData);
           pdf.addPage();
         }
       }
+      pdf.setFontSize(10);
+      pdf.text('Bilti Printing Date: ' + this.printingDate, 107.6, 83);
   
       pdf.save('bilti');
       this.onClose();
-    })
+    });
+  }
+
+  formatCurrentDate(): string {
+    const now = new Date();
+const options: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'short',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  hour12: false
+};
+
+return now.toLocaleString('en-GB', options).replace(',', '');
   }
 
   onClose(){
